aac76797e22392a5136f17c9f6904688c1c912e7
Prasanth Bendra <prasanthbendra@gmail.com>
2016-09-01 02:53:18 +0530

    Webpack config and first react page

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default class App extends React.Component {
+
+    render(){
+        return (<div> It works!! </div>);
+    }
+}
+
+
+
