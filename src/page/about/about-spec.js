a97eb4602cd06b51b3049a8eccd0e8587bba6d53
Prasanth B <PrasanthB@quinnox.com>
2016-09-06 17:53:49 +0530

    mocha chai basic test setup

---
+++ src/page/about/about-spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+let expect = chai.expect;
+
+describe("About component", function(){
+    it("Another react test", function(){
+        expect(5).to.equal(5);
+    });
+    it("My first second test");
+})
---
+++ src/page/pricing/pricing-spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+let expect = chai.expect;
+
+describe("Pricing component", function(){
+    it("My first react test", function(){
+        expect(5).to.equal(5);
+    });
+    it("My first second test");
+})
